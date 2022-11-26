c10687cad32ad7d6f96958cdfa25e601511c1890
Nikita Mix <sklipgame@gmail.com>
2022-11-26 23:09:11 +0300

    i18n bug fix

--- config.py
+++ config.py
@@ -3,7 +3,6 @@ from pathlib import Path
 
 TOKEN = '5703301906:AAHYJIsOC3ZKQk_CdvqLvR6euuG2pKwr-tQ'
 
-
 I18N_DOMAIN = 'YB_translate'
 BASE_DIR = Path(__file__).parent
 LOCALES_DIR = BASE_DIR / 'locales'
