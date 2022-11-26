b9aa1d84a3c52a584f90827720d591a43e9506b0
Nikita Mix <sklipgame@gmail.com>
2022-11-26 23:08:37 +0300

    i18n bug fix

--- config.py
+++ config.py
@@ -5,5 +5,5 @@ TOKEN = '5703301906:AAHYJIsOC3ZKQk_CdvqLvR6euuG2pKwr-tQ'
 
 
 I18N_DOMAIN = 'YB_translate'
-BASE_DIR = os.path.abspath(os.curdir)
-LOCALES_DIR = BASE_DIR + '\\locales'
+BASE_DIR = Path(__file__).parent
+LOCALES_DIR = BASE_DIR / 'locales'
